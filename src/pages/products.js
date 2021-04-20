365c1d52f8f36639f9b4927b1e06acb09338e09e
Ben Borgers <borgersbenjamin@gmail.com>
2021-04-20 17:15:37 -0400

    Add "fall in love with life again" section

--- src/pages/products.js
+++ src/pages/products.js
@@ -30,13 +30,20 @@ export default () => {
             <div className="h-24" />
 
             {/* Brother Screen */}
-            <section className="max-w-screen-xl mx-auto grid grid-cols-2 gap-y-4">
+            <section className="max-w-screen-xl mx-auto grid grid-cols-2 gap-x-16">
                 <div className="mt-4">
-                    <h1 className="text-3xl font-extrabold text-gray-900 mb-8">The <span className="bg-yellow-100 inline-block px-1 italic rounded-lg font-black">all-new</span> Brother Screen®</h1>
+                    <h2 className="text-3xl font-extrabold text-gray-900 mb-8">The all-new <span className="bg-gradient-to-br from-yellow-400 to-red-600 bg-clip-text text-transparent">Brother Screen</span><span className="text-gray-300 font-normal">®</span></h2>
 
-                    <div className="prose sm:prose-lg">
+                    <div className="space-y-2 text-lg">
                         <p>
-                            Gone are the days of wall-mounted Brother Screens.
+                            Gone are the days of wall-mounted Brother Screen.
+                        </p>
+                        <p>
+                            The next generation of Brother Screen comes with you <em>wherever you go</em>. Constantly helpful, ready to keep you on track. Leave your worries about unintentional thought crimes in the 1980s — this is {new Date().getFullYear()}.
+                        </p>
+                        {/* TODO: could include quote about how screens are always monitored, and explanation of panopticon */}
+                        <p>
+                            Representatives from Brother are always monitoring a random subset of screens, as part of our <em>Positive Panopticon</em> program.
                         </p>
                     </div>
                 </div>
@@ -50,6 +57,35 @@ export default () => {
                     </div>
                     <p className="text-white font-handwriting text-3xl transform -rotate-3 absolute top-32 left-6">comes everywhere <br /> with you!</p>
                 </div>
+            </section>
+
+            <div className="h-24" />
+
+            <section
+                className="max-w-2xl mx-auto bg-gray-900 px-16 py-8 rounded-xl shadow-lg text-white"
+                style={{
+                    backgroundImage: `url("data:image/svg+xml,%3Csvg width='100' height='20' viewBox='0 0 100 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M21.184 20c.357-.13.72-.264 1.088-.402l1.768-.661C33.64 15.347 39.647 14 50 14c10.271 0 15.362 1.222 24.629 4.928.955.383 1.869.74 2.75 1.072h6.225c-2.51-.73-5.139-1.691-8.233-2.928C65.888 13.278 60.562 12 50 12c-10.626 0-16.855 1.397-26.66 5.063l-1.767.662c-2.475.923-4.66 1.674-6.724 2.275h6.335zm0-20C13.258 2.892 8.077 4 0 4V2c5.744 0 9.951-.574 14.85-2h6.334zM77.38 0C85.239 2.966 90.502 4 100 4V2c-6.842 0-11.386-.542-16.396-2h-6.225zM0 14c8.44 0 13.718-1.21 22.272-4.402l1.768-.661C33.64 5.347 39.647 4 50 4c10.271 0 15.362 1.222 24.629 4.928C84.112 12.722 89.438 14 100 14v-2c-10.271 0-15.362-1.222-24.629-4.928C65.888 3.278 60.562 2 50 2 39.374 2 33.145 3.397 23.34 7.063l-1.767.662C13.223 10.84 8.163 12 0 12v2z' fill='#374151' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E")`
+                }}
+            >
+                <h2 className="text-4xl font-black text-center mb-8">Fall in love with life again</h2>
+
+                <div className="text-center text-lg space-y-4 text-gray-200">
+                    <p>
+                        Brother Screen recognizes possible problems with your behavior and compliance.
+                    </p>
+                    <p>
+                        If problems are detected, Brother provides rehabilitation, in private and away from the rest of society, free of charge.
+                    </p>
+                    <p>
+                        Brother is on your side, protecting you from others and defending you from yourself.
+                    </p>
+                </div>
+            </section>
+
+            <div className="h-36" />
+
+            <section>
+                controls dangerous speech to regulate society
             </section>
         </Layout>
     )
